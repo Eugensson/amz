e40e137c2fb96153d9d2d9c9ad03e642ec8f0393
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-01-06 10:43:27 +0200

    Created category sidebar

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
-import { MenuIcon } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
 import { Menu } from "@/components/shared/header/menu";
 import { Search } from "@/components/shared/header/search";
+import { Sidebar } from "@/components/shared/header/sidebar";
 
 import data from "@/lib/data";
 import { APP_NAME } from "@/lib/constants";
+import { getAllCategories } from "@/lib/actions/product.actions";
 
-export const Header = () => {
+export const Header = async () => {
+  const categories = await getAllCategories();
   return (
     <header className="bg-black text-white">
       <div className="px-2">
@@ -39,19 +40,13 @@ export const Header = () => {
         </div>
       </div>
       <div className="flex items-center px-3 mb-[1px] bg-gray-800">
-        <Button
-          variant="ghost"
-          className="dark header-button flex items-center gap-1 text-base [&-svg]:size-6"
-        >
-          <MenuIcon />
-          All
-        </Button>
+        <Sidebar categories={categories} />
         <div className="flex items-center flex-wrap gap-3 overflow-hidden max-h-[42px]">
           {data.headerMenus.map((menu) => (
             <Link
               href={menu.href}
               key={menu.href}
-              className="header-button !p-2 "
+              className="header-button !p-2"
             >
               {menu.name}
             </Link>
